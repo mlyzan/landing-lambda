10444b2b33e8e80f5f6ede330c98b27e505a87b3
Volodymyr <mlyzan@ukr.net>
2019-09-17 15:33:14 +0300

    add js.file: add slow scroll, add form to site

---
+++ dist/js/main.js
@@ -0,0 +1,43 @@
+"use strict";
+
+;
+window.addEventListener('DOMContentLoaded', function () {
+  //close modal thanks
+  var close = document.querySelector(".close"),
+      overlay = document.querySelector(".overlay");
+  close.addEventListener('click', function () {
+    overlay.style.display = "none";
+  }); //slow scroll
+
+  var anchor = document.getElementsByTagName('a');
+
+  var _loop = function _loop(i) {
+    anchor[i].addEventListener('click', function (e) {
+      e.preventDefault();
+      var blockID = anchor[i].getAttribute('href');
+      document.querySelector(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    });
+  };
+
+  for (var i = 0; i < anchor.length; i++) {
+    _loop(i);
+  } // submit for form
+
+
+  $('form').submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+    }).done(function () {
+      $(this).find("input").val("");
+      $('.overlay').fadeIn('slow');
+      $('form').trigger('reset');
+    });
+    return false;
+  });
+});
---
+++ src/js/main.js
@@ -0,0 +1,39 @@
+;
+window.addEventListener('DOMContentLoaded', function(){
+   
+   //close modal thanks
+   let close = document.querySelector(".close"),
+    overlay = document.querySelector(".overlay");
+   close.addEventListener('click', ()=>{
+      overlay.style.display = "none";
+   });
+   //slow scroll
+    let anchor = document.getElementsByTagName('a');
+    for (let i = 0; i < anchor.length; i++){
+        anchor[i].addEventListener('click', function (e) {
+          e.preventDefault()
+          
+          const blockID = anchor[i].getAttribute('href')
+          
+          document.querySelector(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+          })
+        })
+      }
+
+    // submit for form
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('.overlay').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+        });
+});
